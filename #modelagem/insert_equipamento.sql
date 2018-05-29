65d55a3efbc89d626dbc245103eb0b682a25b93b
Diego Vicente <diego.vdossantos@gmail.com>
2018-05-29 09:37:22 -0300

    insert de um novo equipamento

---
+++ #modelagem/insert_equipamento.sql
@@ -0,0 +1,9 @@
+INSERT INTO simbi.equipamento_siglas (id, sigla, descricao, roteiro) VALUES (1, 'BMA', 'Biblioteca Mario de Andrade', null);
+
+INSERT INTO simbi.secretarias (id, sigla, descricao) VALUES (1, 'SMC', 'Secretaria Municipal da Cultura');
+
+INSERT INTO simbi.subordinacao_administrativas (id, descricao) VALUES (1, 'Teste Sub. Administrativa');
+
+INSERT INTO simbi.enderecos (id, cep, logradouro, numero, complemento, bairro, cidade, estado, subprefeitura_id, distrito_id, macrorregiao_id, regiao_id, regional_id) VALUES (1, '01302-000', 'Rua da Consolação', 94, null, 'Consolação', 'São Paulo', 'SP', null, null, 1, 5, 6);
+
+INSERT INTO simbi.equipamentos (id, nome, tipo_servico_id, equipamento_sigla_id, secretaria_id, subordinacao_administrativa_id, tematico, nomeTematica, endereco_id, telefone, telecentro, acervo_especializado, nucleo_braile, status_id, publicado) VALUES (1, 'Biblioteca Mario de Andrade', 1, 1, 1, 1, 0, null, 1, '(11) 3775-0002', 0, 0, 0, 1, 1);
