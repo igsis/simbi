57b5eb7ed08f60e555a65c5b525408d80ce0320b
Isaías Mendes <isaiaszmendes@gmail.com>
2018-08-02 11:17:23 -0300

    Organizando View Frequência

--- resources/views/usuarios/frequencia.blade.php
+++ resources/views/usuarios/frequencia.blade.php
@@ -3,7 +3,7 @@
 @section('conteudo')
 
     <div class="col-lg-6 col-lg-offset-3">
-        <h1><i class="glyphicon glyphicon-user"></i>Frequência de Usuário</h1>
+        <h1><i class="glyphicon glyphicon-user"></i> Frequência</h1>
         <hr>
 
         <form method="POST" action="{{route('createFrequencia')}}">
@@ -14,39 +14,51 @@
                 <input class="form-control" type="number" name="evento" value="{{old('evento')}}">
             </div>
 
-            <div class="form-group ">
-                <label for="email">Data</label>
-                <input class="form-control" type="date" name="data" value="{{old('data')}}">
+            <div class="row">
+                <div class="form-group col-md-6">
+                    <div class="form-group ">
+                        <label for="email">Data</label>
+                        <input class="form-control" type="date" name="data" value="{{old('data')}}">
+                    </div>
+                </div>
+                <div class="form-group col-md-6">
+                    <div class="form-group ">
+                        <label for="login">Hora</label>
+                        <input class="form-control" type="text" data-mask="00:00" name="hora" id="hora" placeholder="Digite a hora" value="{{old('hora')}}">
+                    </div>
+                </div>
             </div>
 
-            <div class="form-group ">
-                <label for="login">Hora</label>
-                <input class="form-control" type="text" data-mask="00:00" name="hora" id="hora" value="{{old('hora')}}">
-            </div>
-
-            <div class="form-group ">
-                <label for="email">Criança</label>
-                <input class="form-control" type="number" id="crianca" name="crianca" value="{{old('crianca')}}">
-            </div>
-
-            <div class="form-group ">
-                <label for="jovem">Jovem</label>
-                <input class="form-control" type="number" id="jovem" name="jovem" value="{{old('jovem')}}">
-            </div>
-
-            <div class="form-group ">
-                <label for="adulto">Adulto</label>
+            <div class="row">
+                <div class="form-group col-md-3">
+                     <div class="form-group ">
+                        <label for="email">Criança</label>
+                        <input class="form-control" type="number" id="crianca" name="crianca" value="{{old('crianca')}}">
+                    </div>
+                </div>
+                <div class="form-group col-md-3">
+                     <div class="form-group ">
+                        <label for="jovem">Jovem</label>
+                        <input class="form-control" type="number" id="jovem" name="jovem" value="{{old('jovem')}}">
+                    </div>
+                </div>
+                <div class="form-group col-md-3">
+                     <div class="form-group ">
+                        <label for="adulto">Adulto</label>
                 <input class="form-control" type="number" id="adulto" name="adulto" value="{{old('adulto')}}">
-            </div>
-        
-            <div class="form-group ">
-                <label for="idoso">Idoso</label>
-                <input class="form-control" type="number" id="idoso" name="idoso" value="{{old('idoso')}}">
+                    </div>
+                </div>
+                <div class="form-group col-md-3">
+                     <div class="form-group ">
+                        <label for="idoso">Idoso</label>
+                        <input class="form-control" type="number" id="idoso" name="idoso" value="{{old('idoso')}}">
+                    </div>
+                </div>
             </div>
 
             <div class="form-group ">
                 <label for="total">Total</label>
-                <input class="form-control" id="total" type="number" name="total" value="{{old('total')}}">
+                <input class="form-control" id="total" type="number" name="total" placeholder="Total de Pessoas" value="{{old('total')}}">
             </div>
 
             <div class="form-group ">
