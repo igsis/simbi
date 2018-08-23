d73e7b982c81117558e690dcbc12816180e428d6
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2018-08-23 14:09:45 -0300

    melhorar o form de frequencia de publico atendido

--- resources/views/frequencia/publico/cadastro.blade.php
+++ resources/views/frequencia/publico/cadastro.blade.php
@@ -13,7 +13,7 @@
             {{ csrf_field() }}
 
             <div class="row">
-                <div class="form-group col-xs-12 col-md-10">
+                <div class="form-group col-md-5">
                     <label for="tipoServico">Dia da semana</label>
                     <select class="form-control" name="diaSemana" id="diaSemana">
                         <option value="">Selecione uma Opção</option>
@@ -21,6 +21,7 @@
                             <option value="{{$diaSemana->id}}">{{$diaSemana->dia}}</option>
                         @endforeach
                     </select>
+                </div>
             </div>
 
             <div class="row">
