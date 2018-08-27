7fa97ee1a862920b2cfa747c1d343aa03234c358
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2018-08-27 16:21:54 -0300

    Revert "retirar o link do evento externo"
    This reverts commit 24ba2bd17f2860f8aaf7b16b69add3e6d044d851.

--- resources/views/frequencia/index.blade.php
+++ resources/views/frequencia/index.blade.php
@@ -23,7 +23,7 @@
                     <td>
                         @if($type == 1)
                             <a href="{{ route('frequencia.cadastro', $equipamento->id) }}" class="btn btn-success" style="margin-right: 3px"><i class="glyphicon glyphicon-plus-sign"></i> Evento Interno</a>
-                            <a href="#" class="btn btn-success" style="margin-right: 3px"><i class="glyphicon glyphicon-plus-sign"></i> Evento Externo</a>
+                            <a href="{{ route('frequencia.publico.cadastro', $equipamento->id) }}" class="btn btn-success" style="margin-right: 3px"><i class="glyphicon glyphicon-plus-sign"></i> Evento Externo</a>
                             <a href="{{ route('frequencia.publico.cadastro', $equipamento->id) }}" class="btn btn-success" style="margin-right: 3px"><i class="glyphicon glyphicon-plus-sign"></i> Publico Atendido</a>
                         @else
                             <a href="{{ route('frequencia.listar', $equipamento->id) }}" class="btn btn-warning" style="margin-right: 3px"><i class="glyphicon glyphicon-stats"></i> Exibir Frequência</a>
