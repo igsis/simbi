63bd74c53c641afdcc65c6a0aa7692696717b578
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2018-09-25 12:04:08 -0300

    Arrumando o alerta

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,29 +10,31 @@
         @include('auth.login')
     @else
 		<hr>
-		@if($ocorrencias != null && $frequenciasCadastradas != null)
+		<?php $faltaFrequencia = 0; ?>
+		@if($ocorrencias != null || $frequenciasCadastradas != null)
 			@foreach($ocorrencias as $ocorrencia)
 				@if(!(in_array($ocorrencia->id, $frequenciasCadastradas)) && $ocorrencia->publicado == 1)
 					<?php
-							$dataOcorrencia = strtotime($ocorrencia->data);
-							$diaHoje = strtotime(date('Y-m-d'));
+						$dataOcorrencia = strtotime($ocorrencia->data);
+						$diaHoje = strtotime(date('Y-m-d'));
 
-							$res = $dataOcorrencia - $diaHoje;
+						$res = $dataOcorrencia - $diaHoje;
 
-							$res = $res / 86400;
+						$res = $res / 86400;
 
-							$faltaFrequencia = 0;
-
-							if($res < 0){
-								$faltaFrequencia = 1;
-							}
-
+						if($res < 0){
+							$faltaFrequencia = 1;
+						}
 					?>
 				@endif
 			@endforeach
 			@if($faltaFrequencia == 1)
 				<div class="alert alert-danger" role="alert">
-					<strong>Frequências!</strong> Existem frequências a serem adicionadas.
+					Existem <strong> frequências </strong> a serem adicionadas.
+				</div>
+			@else
+				<div class="alert alert-success" role="alert">
+					Todas as <strong> frequências </strong> estão em dia!
 				</div>
 			@endif
 		@endif
