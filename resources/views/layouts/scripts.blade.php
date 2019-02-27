a7b93da133d1886a0f80103929106f9e8b7265f8
Diego Vicente <diego.vdossantos@gmail.com>
2019-02-27 13:10:10 -0300

    comentando possivel javascript inutil

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -22,7 +22,7 @@
     <script src="{{asset('AdminLTE/plugins/iCheck/icheck.min.js')}}"></script>
     
     <script src="{{asset('js/jquery-1.12.4.min.js')}}"></script>
-    <script src="{{asset('js/bootstrap.js')}}"></script>
+    {{--<script src="{{asset('js/bootstrap.js')}}"></script>--}}
     <script src="{{asset('js/jquery.mask.js')}}"></script>
     <script src="{{asset('js/scripts.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
