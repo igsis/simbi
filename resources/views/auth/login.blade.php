60e7e0745c0baba6933fbc7eec488b51a07c1db4
Diego Vicente <diego.vdossantos@gmail.com>
2019-02-27 12:20:56 -0300

    ajuste tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,33 +6,31 @@
     <div class="login-box-body">
         <p class="login-box-msg">Coloque as informações para o login</p>
 
-        <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+        <form method="POST" action="{{ route('login') }}">
+
             {{ csrf_field() }}
             <div class="form-group has-feedback {{ $errors->has('name') ? ' has-error' : '' }}">
                 <input id="login" type="text" class="form-control" name="login" maxlength="7" value="{{ old('login') }}" required placeholder="Usuário">
                 <span class="glyphicon glyphicon-user form-control-feedback"></span>
                 @if ($errors->has('login'))
                     <span class="help-block">
-                                    <strong>{{ $errors->first('login') }}</strong>
-                                </span>
+                        <strong>{{ $errors->first('login') }}</strong>
+                    </span>
                 @endif
             </div>
+
             <div class="form-group has-feedback {{ $errors->has('password') ? ' has-error' : '' }}">
                 <input id="password" type="password" class="form-control" name="password" required placeholder="Senha">
                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
                 @if ($errors->has('password'))
                     <span class="help-block">
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                </span>
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
                 @endif
             </div>
             <div class="row">
                 <div class="col-xs-8">
-                    <div class="checkbox icheck">
-                        <label>
-                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Mantenha-se Conectado
-                        </label>
-                    </div>
+                    <a href="{{ route('password.request') }}">Esqueci minha Senha</a>
                 </div>
                 <!-- /.col -->
                 <div class="col-xs-4">
@@ -41,10 +39,6 @@
                 <!-- /.col -->
             </div>
         </form>
-
-        <a class="btn btn-link" href="{{ route('password.request') }}">
-            Esqueceu sua Senha?
-        </a>
 
     </div>
     <!-- /.login-box-body -->
