d6ef10fd7a5ff795b5433a88ab223cd834691797
Lucas <lucasdauto79@gmail.com>
2019-03-01 17:59:37 -0300

    Alteração Layout Ocorrencia

--- resources/views/frequencia/cadastroEvento.blade.php
+++ resources/views/frequencia/cadastroEvento.blade.php
@@ -1,68 +1,86 @@
-@extends('layouts.master')
+@extends('layouts.master2')
 
-@section('tituloPagina')
-    <i class="glyphicon glyphicon-flag"></i> Cadastrar Evento <br>
-@endsection
+@section('titulo','Cadastrar Ocorrência')
 
 @section('conteudo')
 
-    <div class="col-lg-6 col-lg-offset-3">
+    <div class="content-wrapper">
 
-        <form method="POST" action="{{route('eventos.gravar', $equipamento->igsis_id) }}">
-            {{ csrf_field() }}
+        <div class="row">
+            <div class="col-xs-12">
+                @includeIf('layouts.erros')
+            </div>
+        </div>
 
-            <div class="form-group">
-                <div class="form-group">
-                    <label for="evento_ocorrencia_id">Nome do Evento</label>
-                    <input class="form-control" type="text" id="nome" name="nome">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1 class="page-header"><i class="glyphicon glyphicon-flag"></i> Cadastrar Evento <br></h1>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <!-- Default box -->
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                </div>
+                <div class="box-body">
+                    <form method="POST" action="{{route('eventos.gravar', $equipamento->igsis_id) }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <div class="form-group">
+                                <label for="evento_ocorrencia_id">Nome do Evento</label>
+                                <input class="form-control" type="text" id="nome" name="nome">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="form-group col-md-4">
+                                <div class="form-group ">
+                                    <label for="email">Tipo Evento</label>
+                                    <select name="tipoEvento" id="tipoEvento" class="form-control">
+                                        <option value="">Selecione...</option>
+                                        @foreach($tipoEvento as $evento)
+                                            <option value="{{ $evento->id }}">{{ $evento->tipo_evento }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <div class="form-group ">
+                                    <label for="jovem">Projeto Especial</label>
+                                    <select name="projetoEspecial" id="projetoEspecial" class="form-control">
+                                        <option value="">Selecione...</option>
+                                        @foreach($projetoEspecial as $projeto)
+                                            <option value=" {{ $projeto->id }}">{{ $projeto->projeto_especial }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <div class="form-group ">
+                                    <label for="adulto">Forma de Contratação</label>
+                                    <select name="contratacao" id="contratacao" class="form-control">
+                                        <option value="">Selecione...</option>
+                                        @foreach($contratacao as $contrata)
+                                            <option value="{{ $contrata->id }}">{{ $contrata->forma_contratacao }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group hidden">
+                            <label for="id">Igsis Evento id</label>
+                            <input type="text" name="igsis_evento_id" id="igsis_evento_id" value="{{ $igsis_evento_id + 1}}">
+                        </div>
+
+                        <button class="btn btn-success"> Cadastrar</button>
+                    </form>
                 </div>
             </div>
-
-            <div class="row">
-                <div class="form-group col-md-4">
-                    <div class="form-group ">
-                        <label for="email">Tipo Evento</label>
-                        <select name="tipoEvento" id="tipoEvento" class="form-control">
-                            <option value="">Selecione...</option>
-                            @foreach($tipoEvento as $evento)
-                                <option value="{{ $evento->id }}">{{ $evento->tipo_evento }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </div>
-                <div class="form-group col-md-4">
-                    <div class="form-group ">
-                        <label for="jovem">Projeto Especial</label>
-                        <select name="projetoEspecial" id="projetoEspecial" class="form-control">
-                            <option value="">Selecione...</option>
-                            @foreach($projetoEspecial as $projeto)
-                                <option value=" {{ $projeto->id }}">{{ $projeto->projeto_especial }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </div>
-                <div class="form-group col-md-4">
-                    <div class="form-group ">
-                        <label for="adulto">Forma de Contratação</label>
-                        <select name="contratacao" id="contratacao" class="form-control">
-                            <option value="">Selecione...</option>
-                            @foreach($contratacao as $contrata)
-                                <option value="{{ $contrata->id }}">{{ $contrata->forma_contratacao }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </div>
-            </div>
-
-            <div class="form-group hidden">
-                <label for="id">Igsis Evento id</label>
-                <input type="text" name="igsis_evento_id" id="igsis_evento_id" value="{{ $igsis_evento_id + 1}}">
-            </div>
-
-            <button class="btn btn-success"> Cadastrar</button>
-        </form>
-
-
+        </section>
     </div>
 
 @endsection
--- resources/views/frequencia/cadastroOcorrencia.blade.php
+++ resources/views/frequencia/cadastroOcorrencia.blade.php
@@ -1,42 +1,64 @@
-@extends('layouts.master')
+@extends('layouts.master2')
 
-@section('tituloPagina')
-    <i class="glyphicon glyphicon-flag"></i> Cadastrar Ocorrência
-    <small>{{ $equipamento->nome }}</small>
-@endsection
-
+@section('titulo','Cadastrar Ocorrência')
 @section('conteudo')
 
-    <div class="col-lg-6 col-lg-offset-3">
+    <div class="content-wrapper">
 
-        <form method="POST" action="{{route('eventos.grava.ocorrencia', [$equipamento->igsis_id, $evento->igsis_evento_id])}}">
-            {{ csrf_field() }}
+        <div class="row">
+            <div class="col-xs-12">
+                @includeIf('layouts.erros')
+            </div>
+        </div>
 
-            <div class="form-group">
-                <div class="form-group">
-                    <label for="nome">Nome do Evento</label>
-                    <input class="form-control" type="text" id="nome" name="nome" readonly value="{{ $evento->nome_evento }}">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1 class="page-header">
+                <i class="glyphicon glyphicon-flag"></i> Cadastrar Ocorrência
+            </h1>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <!-- Default box -->
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">{{ $equipamento->nome }}</h3>
+                </div>
+                <div class="box-body">
+                    <form method="POST" action="{{route('eventos.grava.ocorrencia', [$equipamento->igsis_id, $evento->igsis_evento_id])}}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <div class="form-group">
+                                <label for="nome">Nome do Evento</label>
+                                <input class="form-control" type="text" id="nome" name="nome" readonly value="{{ $evento->nome_evento }}">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <div class="form-group ">
+                                    <label for="data">Data</label>
+                                    <input class="form-control" type="date" name="data" value="{{ old('data') }}" onblur="arrumaData()">
+                                </div>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <div class="form-group ">
+                                    <label for="hora">Hora</label>
+                                    <input class="form-control hora" maxlength="5" type="text" data-mask="00:00" name="hora" id="hora" placeholder="Digite a hora" value="{{ old('hora') }}">
+                                </div>
+                            </div>
+                        </div>
+
+                        <button class="btn btn-success"> Cadastrar</button>
+                    </form>
                 </div>
             </div>
-
-            <div class="row">
-                <div class="form-group col-md-6">
-                    <div class="form-group ">
-                        <label for="data">Data</label>
-                        <input class="form-control" type="date" name="data" value="{{ old('data') }}" onblur="arrumaData()">
-                    </div>
-                </div>
-                <div class="form-group col-md-6">
-                    <div class="form-group ">
-                        <label for="hora">Hora</label>
-                        <input class="form-control hora" maxlength="5" type="text" data-mask="00:00" name="hora" id="hora" placeholder="Digite a hora" value="{{ old('hora') }}">
-                    </div>
-                </div>
-            </div>
-
-            <button class="btn btn-success"> Cadastrar</button>
-        </form>
+        </section>
     </div>
+
 @endsection
 
 @section('scripts_adicionais')
