15772fd5b6e52e0217d7df9391039f66ad3bb86a
Lucas <lucasdauto79@gmail.com>
2019-03-01 13:45:06 -0300

    Ajuste Layout Gentencia(Secretaria/distrito)

--- resources/views/gerenciar/secretarias/index.blade.php
+++ resources/views/gerenciar/secretarias/index.blade.php
@@ -1,99 +1,121 @@
-@extends('layouts.master')
+@extends('layouts.master2')
 
-@section('tituloPagina')
-	<i class="glyphicon glyphicon-cog"></i> Secretarias
+@section('linksAdicionais')
+	@includeIf('links.tabelas_AdminLTE')
 @endsection
+
+@section('titulo','Secretarias')
 
 @section('conteudo')
 
-	{{-- <div class="panel-heading">Página {{ $secretarias->currentPage() }} de {{ $secretarias->lastPage() }}</div> --}}
+	<div class="content-wrapper">
 
-	<div class="form">
-	    <form method="POST" class="form form-inline" action="{{route('searchSecretaria')}}">
-	        {{ csrf_field() }}
-	        <input type="text" name="sigla" class="form-control" placeholder="Sigla" title="Pesquisa pela Sigla">
-	        <input type="text" name="descricao" class="form-control" placeholder="Descrição" title="Pesquisa pela Descrição">
-	        <select name="publicado" class="form-control">
-	        	<option value="1">Selecione</option>
-	        	<option value="1">Ativo</option>
-	        	<option value="0">Desativado</option>
-	        </select>
-	        <button class="btn btn-primary"><i class="glyphicon glyphicon-search"></i></button>
-	    </form>
-	</div><br>
+		<div class="row">
+			<div class="col-xs-12">
+				@includeIf('layouts.erros')
+			</div>
+		</div>
 
-	<div class="table-responsive">
-	    <table class="table table-bordered table-striped ">
-		<thead>
-			<tr>
-				<th>Sigla</th>
-				<th>Descrição</th>
-				<th>Ações</th>
-			</tr>
-		</thead>
-		<tbody>
-			@foreach($secretarias as $secretaria)
-			<tr>
-				<td>{{$secretaria->sigla}}</td>
-				<td>{{$secretaria->descricao}}</td>
-				<td>
-					<button class="btn btn-info" data-toggle="modal" data-target="#secretaria"
-							data-id="{{$secretaria->id}}"
-							data-sigla="{{$secretaria->sigla}}"
-							data-descricao="{{$secretaria->descricao}}">
-						<i class="glyphicon glyphicon-pencil"> </i> Editar
-					</button>
-					<button class="btn btn-danger" data-toggle="modal" data-target="#desativar"
-								data-id="{{$secretaria->id}}"
-								data-title="{{$secretaria->sigla}}"
-								data-route="{{route('deleteSecretaria', '')}}">
-						<i class="glyphicon glyphicon-remove"></i> Excluir
-					</button>
-				</td>
-			</tr>
-			@endforeach
-		</tbody>
-		</table>
-	</div>
-	<button class="btn btn-success" data-toggle="modal" data-target="#secretaria"><i class="glyphicon glyphicon-plus"> Adicionar</i></button> 	
+		<!-- Content Header (Page header) -->
+		<section class="content-header">
+			<h1 class="page-header">
+				<i class="glyphicon glyphicon-cog"></i> Secretarias
+			</h1>
+		</section>
 
-	<!-- Editar Secretaria -->
-	<div class="modal fade" id="secretaria" role="dialog" aria-hidden="true">
-		<div class="modal-dialog modal-dialog-centered">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-					<h4 class="modal-title"></h4>
+		<!-- Main content -->
+		<section class="content">
+
+			<!-- Default box -->
+			<div class="box box-primary">
+				<div class="box-header with-border">
+					<h3 class="box-title">Pesquisa de secretaria</h3>
 				</div>
-				<div class="modal-body">
-					<form method="POST"> {{-- action Pelo js --}}
-						{{csrf_field()}}
-						<input type="hidden" name="id">
-						<div class="form-group">
-							<label>Sigla</label>
-							<input class="form-control" type="text" name="sigla">
-						</div>
-						<div class="form-group">
-							<label for="descricao">Descrição</label>
-							<input class="form-control" type="text" name="descricao">
-						</div>
+				<div class="box-body">
+
+					<div class="table-responsive">
+						<table id="tabela1" class="table table-bordered table-striped ">
+							<thead>
+							<tr>
+								<th>Sigla</th>
+								<th>Descrição</th>
+								<th>Ações</th>
+							</tr>
+							</thead>
+							<tbody>
+							@foreach($secretarias as $secretaria)
+								<tr>
+									<td>{{$secretaria->sigla}}</td>
+									<td>{{$secretaria->descricao}}</td>
+									<td>
+										<button class="btn btn-info" data-toggle="modal" data-target="#secretaria"
+												data-id="{{$secretaria->id}}"
+												data-sigla="{{$secretaria->sigla}}"
+												data-descricao="{{$secretaria->descricao}}">
+											<i class="glyphicon glyphicon-pencil"> </i> Editar
+										</button>
+										<button class="btn btn-danger" data-toggle="modal" data-target="#desativar"
+												data-id="{{$secretaria->id}}"
+												data-title="{{$secretaria->sigla}}"
+												data-route="{{route('deleteSecretaria', '')}}">
+											<i class="glyphicon glyphicon-remove"></i> Excluir
+										</button>
+									</td>
+								</tr>
+							@endforeach
+							</tbody>
+							<tfooter>
+								<tr>
+									<th>Sigla</th>
+									<th>Descrição</th>
+									<th>Ações</th>
+								</tr>
+							</tfooter>
+						</table>
+					</div>
+
+					<button class="btn btn-success" data-toggle="modal" data-target="#secretaria"><i class="glyphicon glyphicon-plus"> Adicionar</i></button>
+				</div>
+			</div>
+		</section>
+
+
+
+		<!-- Editar Secretaria -->
+		<div class="modal fade" id="secretaria" role="dialog" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+						<h4 class="modal-title"></h4>
+					</div>
+					<div class="modal-body">
+						<form method="POST"> {{-- action Pelo js --}}
+							{{csrf_field()}}
+							<input type="hidden" name="id">
+							<div class="form-group">
+								<label>Sigla</label>
+								<input class="form-control" type="text" name="sigla">
+							</div>
+							<div class="form-group">
+								<label for="descricao">Descrição</label>
+								<input class="form-control" type="text" name="descricao">
+							</div>
 					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-						<input type="submit" class="btn btn-success" name="novaSecretaria" value="">
+						<input type="submit" class="btn btn-success" name="novaSecretaria" value="Adicionar">
 					</div>
-				</form>
+					</form>
+				</div>
 			</div>
 		</div>
+		@include('layouts.desativar')
+
 	</div>
-	@include('layouts.desativar')
-<div class="text-center">
-	@if(isset($dataForm))
-        {!! $secretarias->appends($dataForm)->links() !!}
-    @else
-        {!! $secretarias->links() !!}
-    @endif
-</div>
+
+	<!-- fim codigo novo -->
+
 @endsection
 @section('scripts_adicionais')
     <script type="text/javascript">
@@ -123,6 +145,7 @@
         });
     </script>
 
+	@include('scripts.tabelas_admin')
     @include('scripts.desativar_modal')
 
 @endsection
