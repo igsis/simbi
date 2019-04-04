cece645b922d434c96c64061382c8785d2b3d9e0
Lucas <lucasdauto79@gmail.com>
2019-04-04 00:16:11 -0300

    Update editar.blade.php

--- resources/views/funcionarios/editar.blade.php
+++ resources/views/funcionarios/editar.blade.php
@@ -318,7 +318,7 @@
             if (txtVal !== "")
             {
                 newOption.appendChild(newOptionVal);
-                newOption.setAttribute("value",`${i + 1}`);
+                newOption.setAttribute("value", `${i + 1}`);
                 select.insertBefore(newOption, select.lastChild);
                 newOption.setAttribute('selected', 'selected');
 
