0979da0dc276fb75121dbad847134f62191837e3
Lucas <lucasdauto79@gmail.com>
2019-04-23 14:13:38 -0300

    Correção de Usuarios index

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -4,11 +4,22 @@ namespace Simbi\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Simbi\Http\Controllers\Controller;
+use Simbi\Models\Equipamento;
+use Simbi\Models\Funcionario;
+use Simbi\Models\User;
 
 class FuncionarioController extends Controller
 {
+    public function index(Request $types)
+    {
+        $type = $types->type;
+        $users = Funcionario::where('publicado', '=', $type)->orderBy('id')->paginate(10);
+        $equipamentos = Equipamento::all();
+        return view('funcionarios.index', compact('users', 'equipamentos','type'));
+    }
+
     // Filtro de Usuários Ativados
-    public function searchFuncionario(Request $request, User $user)
+    public function searchUser(Request $request, User $user)
     {
         $dataForm = $request->except('_token');
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,77 @@ class User extends Authenticatable
     {
         return $this->belongsTo(PerguntaSeguranca::class);
     }
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
+
+    public function cargo()
+    {
+        return $this->belongsTo(Cargo::class);
+    }
+
+    public function escolaridade()
+    {
+        return $this->belongsTo(Escolaridade::class);
+    }
+
+    /**
+     * Função de relacionamento entre Usuarios e Equipamentos
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function equipamentos()
+    {
+        return $this->belongsToMany(Equipamento::class)->using(EquipamentoUser::class);
+    }
+
+    public function frequencias()
+    {
+        // return $this->hasManyThrough(Frequencia::class, Evento::class);
+
+        return $this->hasMany(Frequencia::class);
+    }
+
+    public function frequenciasPublicos()
+    {
+        return $this->hasMany(FrequenciasPublico::class);
+    }
+
+    public function frequenciasPortarias()
+    {
+        return $this->hasMany(FrequenciasPortaria::class);
+    }
+
+    public function funcao()
+    {
+        return $this->belongsTo(Funcao::class);
+    }
+
+    public function ocorrencia()
+    {
+        return $this->hasMany(EquipamentoOcorrencia::class);
+    }
+
+    public function search(Array $data)
+    {
+        return $this->where(function ($query) use($data)
+        {
+            $query->where('publicado', $data['types']);
+
+            if (isset($data['name'])) {
+                $query->where('name', $data['name']);
+            }
+
+            if (isset($data['login'])) {
+                $query->where('login', $data['login']);
+            }
+
+            if (isset($data['email'])) {
+                $query->where('email', $data['email']);
+            }
+
+        });
+
+    }
+
 }
