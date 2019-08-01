6408073f2aaf9023f0040e2c4beb3b74ff7c148a
lucasdauto <31465480+lucasdauto@users.noreply.github.com>
2019-08-01 18:16:19 -0300

    Mensagens de erro

--- resources/views/layouts/erros.blade.php
+++ resources/views/layouts/erros.blade.php
@@ -15,6 +15,16 @@
     </div>
 @endif
 
+@if(Session::has('flash_message_danger'))
+    <div  style="margin: 15px;">
+        <div class="alert alert-danger alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <h4><i class="icon fa fa-ban"></i> Erro!</h4>
+            {!! session('flash_message_danger') !!}
+        </div>
+    </div>
+@endif
+
 @if (count($errors) > 0)
     <div class="alert alert-danger alert-dismissible">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
